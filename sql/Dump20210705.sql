e6a6fca2b7f420f9a8fe8c7533204637e8b6fa57
PamelaAbrego <71997534+PamelaAbrego@users.noreply.github.com>
2021-07-06 10:52:41 -0600

    Update Dump20210705.sql

--- sql/Dump20210705.sql
+++ sql/Dump20210705.sql
@@ -78,8 +78,8 @@ CREATE TABLE `usuarios` (
   `id` int NOT NULL AUTO_INCREMENT,
   `user` varchar(45) NOT NULL,
   `email` varchar(45) NOT NULL,
-  `password` varchar(45) NOT NULL,
-  `salt` varchar(45) NOT NULL,
+  `password` varchar(1000) NOT NULL,
+  `salt` varchar(1000) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */
