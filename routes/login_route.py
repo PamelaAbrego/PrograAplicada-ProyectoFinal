90fbac83ca220d3e5832ed4868222f3f678c9145
Pamela Abrego <pamelabregor@gmail.com>
2021-07-15 22:50:01 -0600

    Login corrección

--- routes/login_route.py
+++ routes/login_route.py
@@ -13,19 +13,24 @@ class Login:
             elif request.method == "POST":
                 logic = UsuariosLogic()
                 userName = request.form["user"]
-                password = request.form["password"]
-                userDict = logic.getUserByName(userName)
-                role = userDict["role"]
-                salt = userDict["salt"].encode("utf-8")
-                hashPasswd = bcrypt.hashpw(password.encode("utf-8"), salt)
-                dbPasswd = userDict["password"].encode("utf-8")
-                if hashPasswd == dbPasswd:
-                    session["login_user"] = userName
-                    session["login_email"] = logic.getEmailByName(userName)
-                    session["login_role"] = role
-                    session["loggedIn"] = True
-                    return redirect("principal")
+                result = logic.checkUser(userName)
+                if len(result) > 0:    
+                    password = request.form["password"]
+                    userDict = logic.getUserByName(userName)
+                    role = userDict["role"]
+                    salt = userDict["salt"].encode("utf-8")
+                    hashPasswd = bcrypt.hashpw(password.encode("utf-8"), salt)
+                    dbPasswd = userDict["password"].encode("utf-8")
+                    if hashPasswd == dbPasswd:
+                        session["login_user"] = userName
+                        session["login_email"] = logic.getEmailByName(userName)
+                        session["login_role"] = role
+                        session["loggedIn"] = True
+                        return redirect("principal")
+                    else:
+                        flash("Contraseña incorrecta", "login")
+                        return redirect("login")
+                    return "posted login"
                 else:
-                    flash("Contraseña incorrecta", "login")
+                    flash("El usuario ingresado no existe.", "login")
                     return redirect("login")
-                return "posted login"
